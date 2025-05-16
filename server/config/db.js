6a11baec613494fc4015c1f462e957fbd1fc66b0
sanjaysaini383 <sanjaysaini4423@gmail.com>
2025-05-16 20:58:15 +0530

    some changes in db

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,13 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         mongoose.set('strictQuery', false);
-        const conn = await mongoose.connect(process.env.MONGODB_URI);
+        const conn = await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            ssl: true,
+            tlsInsecure: false,
+        });
+
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     }
     catch (error) {
