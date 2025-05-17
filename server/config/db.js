7cdebd3dae44d5e2cc38183fc0d6664f8086594c
sanjaysaini383 <sanjaysaini4423@gmail.com>
2025-05-17 08:29:05 +0530

    db js

--- server/config/db.js
+++ server/config/db.js
@@ -2,12 +2,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         mongoose.set('strictQuery', false);
-        const conn = await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            ssl: true,
-            tlsInsecure: false,
-        });
+        const conn = await mongoose.connect(process.env.MONGODB_URI);
 
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     }
